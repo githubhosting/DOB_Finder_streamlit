d3ad6ae222c2e71711f843f02b10f5d9c5319942
myselfshravan <shravanrevanna@gmail.com>
2022-12-05 01:11:25 +0530

    auth added

--- sis.py
+++ sis.py
@@ -222,6 +222,55 @@ def micro(head: str, year: str, dept: str, rollnum):
 
 
 if __name__ == '__main__':
+    def check_password():
+        """Returns `True` if the user had the correct password."""
+
+        def password_entered():
+            """Checks whether a password entered by the user is correct."""
+            if st.session_state["password"] == st.secrets["password"]:
+                st.session_state["password_correct"] = True
+                del st.session_state["password"]
+            else:
+                st.session_state["password_correct"] = False
+
+        if "password_correct" not in st.session_state:
+            # First run, show input for password.
+            st.text_input(
+                "Enter the Password", type="password", on_change=password_entered, key="password"
+            )
+            return False
+        elif not st.session_state["password_correct"]:
+            # Password not correct, show input + error.
+            st.text_input(
+                "Password", type="password", on_change=password_entered, key="password"
+            )
+            st.error("😕 Password incorrect")
+            return False
+        else:
+            # Password correct.
+            return True
+
+
+    if check_password():
+        HEAD = "1MS"
+        st.title("Find Anyone's Data just using USN")
+        usn = st.text_input("Enter your USN")
+        check = False
+        if len(usn) == 10:
+            check = True
+        btn = st.button("Find DOB")
+        cgpa = st.button("Find CGPA")
+        if check & btn:
+            roll = int(usn[7:10])
+            DEPT = usn[5:7].upper()
+            YEAR = usn[3:5]
+            macro(HEAD, YEAR, DEPT, dry=False)
+        if cgpa:
+            roll = int(usn[7:10])
+            DEPT = usn[5:7].upper()
+            YEAR = usn[3:5]
+            micro(HEAD, YEAR, DEPT, roll)
+
     hide_streamlit_style = """
                 <style>
                 #MainMenu {visibility: hidden;}
@@ -238,21 +287,3 @@ if __name__ == '__main__':
                 </style>
                 """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-    HEAD = "1MS"
-    st.title("Find Anyone's Data just using USN")
-    usn = st.text_input("Enter your USN")
-    check = False
-    if len(usn) == 10:
-        check = True
-    btn = st.button("Find DOB")
-    if check or btn:
-        roll = int(usn[7:10])
-        DEPT = usn[5:7].upper()
-        YEAR = usn[3:5]
-        macro(HEAD, YEAR, DEPT, dry=False)
-        cgpa = st.button("Find CGPA")
-        if cgpa:
-            micro(HEAD, YEAR, DEPT, roll)
-
-    else:
-        st.write("Enter a valid USN")
