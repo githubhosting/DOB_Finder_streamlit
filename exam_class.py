87e974c8f78c855a8b73d2bc7c04c79bf12dc2e6
myselfshravan <shravanrevanna@gmail.com>
2022-12-04 23:18:16 +0530

    combined exam and sis

--- exam_class.py
+++ exam_class.py
@@ -1,5 +1,3 @@
-import os
-import tempfile
 from urllib.parse import urlparse
 import streamlit as st
 from scraper import Scraper, gen_usn, roll_range
@@ -30,8 +28,7 @@ class ExamScraper(Scraper):
             "photo": f"{url[0]}://{url[1]}" + body.find_all("img")[1]['src'],
         }
 
-    def get_dept(self, head: str, year: str, dept: str, _range=None, tolerate: int = 5):
-        if _range is None: _range = roll_range()
+    def get_dept(self, head: str, year: str, dept: str, tolerate: int = 5):
         payload = gen_payload()
         tol = tolerate
         if tol <= 0: return
@@ -53,40 +50,9 @@ def gen_payload() -> dict[str, str]:
     }
 
 
-def macro(head: str, year: str, dept: str, file=None, dry: bool = False):
-    if not os.path.exists(f"results/{dept}") and not dry: os.mkdir(f"results/{dept}")
-    file = f"results/{dept}/results_{dept}_{year}.csv" if file is None else file
-    with ExamScraper("https://exam.msrit.edu/eresultseven/") as EXAM, \
-            tempfile.TemporaryFile("w+") if dry else open(file, "w+") as f:
-        write = \
-            f"{'usn':{len(head + year + dept) + 3 + 5}}," \
-            f"{'name':64}," \
-            f"{'sgpa':5}," \
-            f"photo," \
-            f""
-        if not dry: f.write(write + "\n")
-        print(write)
-
-        for stat in EXAM.get_dept(head, year, dept):
-            write = \
-                f"{stat['usn']:{len(head + year + dept) + 3 + 5}}," \
-                f"{stat['name']:64}," \
-                f"{stat['sgpa']:5}," \
-                f"{stat['photo']}," \
-                f""
-            print(write)
-
-
-def micro(head: str, year: str, dept: str, start, stop=None):
+def micro(head: str, year: str, dept: str, rollnum):
     with ExamScraper("https://exam.msrit.edu/eresultseven/") as EXAM:
-        write = \
-            f"{'usn':{len(head + year + dept) + 3 + 5}}," \
-            f"{'name':64}," \
-            f"{'sgpa':5}," \
-            f"photo," \
-            f""
-        print(write)
-        for stat in EXAM.get_dept(head, year, dept, roll_range(start, stop)):
+        for stat in EXAM.get_dept(head, year, dept, rollnum):
             write = \
                 f"{stat['usn']:{len(head + year + dept) + 3 + 5}}," \
                 f"{stat['name']:64}," \
@@ -101,7 +67,7 @@ def micro(head: str, year: str, dept: str, start, stop=None):
 
 if __name__ == '__main__':
     HEAD = "1MS"
-    st.title("Find Anyones CGPA just using USN")
+    st.title("Find Anyone's CGPA just using USN")
     usn = st.text_input("Enter your USN")
     check = False
     if len(usn) == 10:
--- sis.py
+++ sis.py
@@ -3,10 +3,10 @@ import time
 from datetime import date
 from typing import Union
 from threading import Thread
-
+from urllib.parse import urlparse
 import streamlit as st
+
 from scraper import Scraper, cached, gen_usn
-import streamlit.components.v1 as components
 
 
 def get_cache():
@@ -76,7 +76,6 @@ class SisScraper(Scraper):
         stats = self.get_stats(payload)
         if not stats:
             tol -= 1
-        tol = tolerate
         yield stats
 
     @cached(get_cache())
@@ -161,6 +160,67 @@ def macro(head: str, year: str, dept: str, file=None, dry: bool = False):
             SIS.save_cache()
 
 
+class ExamScraper(Scraper):
+    def __init__(self, url="https://exam.msrit.edu/"):
+        self.URL = url + ("/" if url[-1] != "/" else "")
+        super(ExamScraper, self).__init__()
+
+    def get_post_body(self, payload):
+        soup = self.get_soap(self.URL, "POST", payload)
+        body = soup.body
+        try:
+            _ = body.find_all("h3")[0].text
+            return body
+        except IndexError:
+            return
+
+    def get_stats(self, payload) -> dict[str, str]:
+        body = self.get_post_body(payload)
+        if body is None: return {}
+        url = urlparse(self.URL)
+        return {
+            "usn": payload["usn"],
+            "name": body.find_all("h3")[0].text,
+            "sgpa": body.find_all("p")[3].text,
+            "photo": f"{url[0]}://{url[1]}" + body.find_all("img")[1]['src'],
+        }
+
+    def get_dept(self, head: str, year: str, dept: str, tolerate: int = 5):
+        payload = gen_payload1()
+        tol = tolerate
+        if tol <= 0: return
+        payload["usn"] = gen_usn(year, dept, roll, head)
+        stats = self.get_stats(payload)
+        if not stats:
+            tol -= 1
+        yield stats
+
+
+def gen_payload1() -> dict[str, str]:
+    return {
+        "usn": "",
+        "osolCatchaTxt": "",
+        "osolCatchaTxtInst": "0",
+        "option": "com_examresult",
+        "task": "getResult"
+    }
+
+
+def micro(head: str, year: str, dept: str, rollnum):
+    with ExamScraper("https://exam.msrit.edu/eresultseven/") as EXAM:
+        for stat in EXAM.get_dept(head, year, dept, rollnum):
+            write = \
+                f"{stat['usn']:{len(head + year + dept) + 3 + 5}}," \
+                f"{stat['name']:64}," \
+                f"{stat['sgpa']:5}," \
+                f"{stat['photo']}," \
+                f""
+            print(write)
+            profile_image = f"{stat['photo']}"
+            st.image(profile_image, caption=stat['name'], use_column_width=True)
+            st.subheader(f'CGPA: {stat["sgpa"]}')
+
+
 if __name__ == '__main__':
     hide_streamlit_style = """
                 <style>
@@ -179,8 +239,6 @@ if __name__ == '__main__':
                 """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
     HEAD = "1MS"
-    # password = st.text_input("Enter Password", type="password")
-    # if password == "shravan1234":
     st.title("Find Anyone's Data just using USN")
     usn = st.text_input("Enter your USN")
     check = False
@@ -192,5 +250,9 @@ if __name__ == '__main__':
         DEPT = usn[5:7].upper()
         YEAR = usn[3:5]
         macro(HEAD, YEAR, DEPT, dry=False)
+        cgpa = st.button("Find CGPA")
+        if cgpa:
+            micro(HEAD, YEAR, DEPT, roll)
+
     else:
         st.write("Enter a valid USN")
